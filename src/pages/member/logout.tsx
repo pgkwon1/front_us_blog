27899294819dfd1898d3a5fbf0c876a70b102320
pgkwon1@naver.com <pgkwon1@naver.com>
2023-03-31 03:23:43 +0900

    Feat: 로그아웃시 userId state 업데이트 하는 기능

--- src/pages/member/logout.tsx
+++ src/pages/member/logout.tsx
@@ -1,12 +1,13 @@
-import { setLoginState } from "@/store/actions/user";
+import { setCurrentUserId, setLoginState } from "@/store/actions/user";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
-export default function logout() {
+export default function Logout() {
   const dispatch = useDispatch();
   const { push } = useRouter();
   useEffect(() => {
     dispatch(setLoginState(0));
+    dispatch(setCurrentUserId(""));
     localStorage.removeItem("token");
     push("/");
   }, []);
