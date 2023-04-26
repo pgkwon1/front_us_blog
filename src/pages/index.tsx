4bc40b3f751fde135bf2c6624c757a91223656d8
pgkwon1@naver.com <pgkwon1@naver.com>
2023-04-27 00:39:14 +0900

    Feat: 메인페이지 글 쓰기 섹션 추가.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,26 @@
 import Posts from "@/components/blog/posts";
+import { Box } from "@mui/material";
+import Link from "next/link";
 
 function Index({ apiConfig }: any) {
   return (
     <>
+      <Link href={"/post/write"}>
+        <Box
+          sx={{
+            padding: "1rem",
+            background: "#fff",
+            marginBottom: "1rem",
+            borderRadius: "10px",
+          }}
+        >
+          <Box
+            sx={{ color: "#857d7d", padding: "1.5rem", background: "#e9e9e9" }}
+          >
+            오늘 공부한 것을 기록해보세요!
+          </Box>
+        </Box>
+      </Link>
       <Posts />
     </>
   );
