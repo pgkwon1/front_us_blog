29a48c1072e1f8f99b9b5cc41a0e99ded478b33e
pgkwon1@naver.com <pgkwon1@naver.com>
2023-05-06 23:36:05 +0900

    refactor : 메뉴 리팩토링 및 아이콘 추가.

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,20 +1,59 @@
-import * as React from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
+import BusinessIcon from "@mui/icons-material/Business";
+import CodeIcon from "@mui/icons-material/Code";
+import CoffeeIcon from "@mui/icons-material/Coffee";
+
 import Link from "next/link";
 import { useSelector } from "react-redux";
 
 export default function Header() {
   const { login_state } = useSelector((state) => state.userReducer);
+  const menuList = [
+    {
+      menuName: "Home",
+      icon: null,
+      href: "/",
+    },
+    {
+      menuName: "기술",
+      icon: <CodeIcon />,
+      href: "/post/category/기술",
+    },
+    {
+      menuName: "직장",
+      icon: <BusinessIcon />,
+      href: "/post/category/직장",
+    },
+    {
+      menuName: "잡담",
+      icon: <CoffeeIcon />,
+      href: "/post/category/잡담",
+    },
+  ];
+  let addMenu;
+  if (login_state === 1) {
+    addMenu = {
+      menuName: "Logout",
+      icon: null,
+      href: "/member/logout",
+    };
+  } else {
+    addMenu = {
+      menuName: "Login",
+      icon: null,
+      href: "/member/login",
+    };
+  }
+  menuList.push(addMenu);
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" sx={{ background: "#fff" }}>
-        <Toolbar>
+        <Toolbar sx={{ gap: "1rem" }}>
           <IconButton
             size="large"
             edge="start"
@@ -24,20 +63,15 @@ export default function Header() {
           >
             <MenuIcon />
           </IconButton>
-          <Link href={"/"}>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              Home
-            </Typography>
-          </Link>
-          {login_state === 1 ? (
-            <Link href={"/member/logout"}>
-              <Button color="inherit">Logout</Button>
-            </Link>
-          ) : (
-            <Link href={"/member/login"}>
-              <Button color="inherit">Login</Button>
-            </Link>
-          )}
+          {menuList.map((menu, index) => {
+            return (
+              <Link key={index} href={menu.href}>
+                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                  {menu.menuName} {menu.icon !== null ? menu.icon : ""}
+                </Typography>
+              </Link>
+            );
+          })}
         </Toolbar>
       </AppBar>
     </Box>
