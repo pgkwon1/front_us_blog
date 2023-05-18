d746c146562dcb8df64ef14c5e5783fe8549abc0
pgkwon1@naver.com <pgkwon1@naver.com>
2023-05-19 01:20:40 +0900

    Fix: 확장자 잘못된 부분 수정

--- src/dto/posts/EditorDto.tsx
+++
@@ -1,7 +0,0 @@
-import { RefObject } from "react";
-import ReactQuill from "react-quill";
-
-export interface IEditorProps {
-  editorRef: RefObject<ReactQuill>;
-  handleContents: (contents: string) => void;
-}
