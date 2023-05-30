63ab69cabf66028a8a99c658aec475bdb664e58e
pgkwon1@naver.com <pgkwon1@naver.com>
2023-05-31 00:25:35 +0900

    Feat: 푸터 추가.

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,3 +1,9 @@
+import { Box } from "@mui/material";
+
 export default function Footer() {
-  return <>Footer</>;
+  return (
+    <Box sx={{ padding: "1.8rem", backgroundColor: "black", color: "white" }}>
+      (C) 2023 PGKWON1. All rights reserved.
+    </Box>
+  );
 }
