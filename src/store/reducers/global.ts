060db23779ca214fe2b2d61dd4c802aa1dddced7
pgkwon1@naver.com <pgkwon1@naver.com>
2023-08-11 18:23:41 +0900

    Feat: 전역 리듀서 추가.

--- src/store/reducers/global.ts
+++ src/store/reducers/global.ts
@@ -5,14 +5,21 @@ const globalReducer = createSlice({
   initialState: {
     isError: false,
     errorMessage: "",
+    isNotice: false,
+    noticeMessage: "",
   },
   reducers: {
     setErrorInfo: (state, action) => {
       state.errorMessage = action.payload.errorMessage;
       state.isError = action.payload.isError;
     },
+
+    setNoticeInfo: (state, action) => {
+      state.isNotice = action.payload.isNotice;
+      state.noticeMessage = action.payload.noticeMessage;
+    },
   },
 });
 
-export const { setErrorInfo } = globalReducer.actions;
+export const { setErrorInfo, setNoticeInfo } = globalReducer.actions;
 export default globalReducer.reducer;
